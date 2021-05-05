70b271e72dd1543c7a208dda886589817d212abf
William Nolen <wpncomps@yahoo.com>
2021-05-05 17:23:21 -0400

    Added abstract methods for the logic gate class

--- DigitalLogicWorkspace/src/LogicGates/LogicGate.java
+++ DigitalLogicWorkspace/src/LogicGates/LogicGate.java
@@ -1,14 +1,30 @@
 package LogicGates;
 
+import processing.core.PVector;
+
 abstract class LogicGate {
 
 	private String type;
-	private int[] inputs;
-	private int[] outputs;
+	private boolean[] inputs;
+	private boolean[] outputs;
+	private PVector coords;
+	private boolean hovered;
 	
 	public abstract void update();
 	
 	public abstract void draw();
+	
+	public boolean getOutput(int channel) {
+		
+		return outputs[channel];
+		
+	}
+	
+	public void setInput(int channel, boolean value) {
+		inputs[channel] = value;
+	}
+	
+	abstract void compute();
 	
 	
 	
