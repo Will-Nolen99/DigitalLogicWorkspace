16bb3530411fbb6fe1dbfcd8170329540e346491
William Nolen <wpncomps@yahoo.com>
2021-05-05 03:22:04 -0400

    Added info page with planned color values for the project

---
+++ DigitalLogicWorkspace/src/DigitalWorkspace.java
@@ -0,0 +1,25 @@
+import processing.core.PApplet;
+
+public class DigitalWorkspace extends PApplet {
+
+    
+    public static void main(String[] args) {
+        PApplet.main("DigitalWorkspace");
+    }
+
+    
+    public void settings(){
+        size(1920, 1080);
+    }
+
+    
+    public void setup(){
+        background(0);
+        stroke(255);
+        strokeWeight(10);
+    }
+
+    public void draw(){
+    	background(0);
+    }
+}
